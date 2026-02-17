5e6eeb2ae33ce7dfaa406968e7e4dd3d5e95dc46
dilshadbrhm <dilsadvi@code.edu.az>
2026-02-17 23:37:39 +0400

    Add Views and Order Controller

---
+++ HospitalManagementSystem/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using HospitalManagement.Application.Interfaces;
+using HospitalManagement.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Order> orders = await _orderRepository.GetAllAsync();
+            return View(orders);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Order order = await _orderRepository.GetByIdWithItemsAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateStatus(int id, string status)
+        {
+            await _orderRepository.UpdateStatusAsync(id, status);
+            TempData["Success"] = "Order status updated";
+            return RedirectToAction("Details", new { id = id });
+        }
+    }
+}
--- HospitalManagementSystem/obj/Debug/net8.0/HospitalManagementSystem.AssemblyInfo.cs
+++ HospitalManagementSystem/obj/Debug/net8.0/HospitalManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd62c6aa5e5ef71ace71cc9d2f3d2fb7bf601e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d5469a9ba5b86acb2aae0e465c48b58245d00c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
